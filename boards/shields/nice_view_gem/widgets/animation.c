41d343d80a99ec298d48796580bea3fff9804e44
cn669600 <christoph.niederer@broadcom.com>
2025-12-28 12:45:23 +0100

    feat(mario): Mario animation

--- boards/shields/nice_view_gem/widgets/animation.c
+++ boards/shields/nice_view_gem/widgets/animation.c
@@ -2,27 +2,53 @@
 #include <zephyr/kernel.h>
 #include "animation.h"
 
-LV_IMG_DECLARE(crystal_01);
-LV_IMG_DECLARE(crystal_02);
-LV_IMG_DECLARE(crystal_03);
-LV_IMG_DECLARE(crystal_04);
-LV_IMG_DECLARE(crystal_05);
-LV_IMG_DECLARE(crystal_06);
-LV_IMG_DECLARE(crystal_07);
-LV_IMG_DECLARE(crystal_08);
-LV_IMG_DECLARE(crystal_09);
-LV_IMG_DECLARE(crystal_10);
-LV_IMG_DECLARE(crystal_11);
-LV_IMG_DECLARE(crystal_12);
-LV_IMG_DECLARE(crystal_13);
-LV_IMG_DECLARE(crystal_14);
-LV_IMG_DECLARE(crystal_15);
-LV_IMG_DECLARE(crystal_16);
+LV_IMG_DECLARE(mario1standing);
+LV_IMG_DECLARE(mario2run1);
+LV_IMG_DECLARE(mario2run2);
+LV_IMG_DECLARE(mario2run3);
+LV_IMG_DECLARE(mario3jump1);
+LV_IMG_DECLARE(mario3jump2);
+LV_IMG_DECLARE(mario3jump3);
+LV_IMG_DECLARE(mario3jump4);
+LV_IMG_DECLARE(mario3jump5);
+LV_IMG_DECLARE(mario3jump6);
+LV_IMG_DECLARE(mario3jump7);
+LV_IMG_DECLARE(mario3jump8);
+LV_IMG_DECLARE(mario3jump9);
+LV_IMG_DECLARE(mario3jump10);
+LV_IMG_DECLARE(mario3jump11);
+LV_IMG_DECLARE(mario3jump12);
+LV_IMG_DECLARE(mario3jump13);
 
 const lv_img_dsc_t *anim_imgs[] = {
-    &crystal_01, &crystal_02, &crystal_03, &crystal_04, &crystal_05, &crystal_06,
-    &crystal_07, &crystal_08, &crystal_09, &crystal_10, &crystal_11, &crystal_12,
-    &crystal_13, &crystal_14, &crystal_15, &crystal_16,
+    &mario1standing,&mario1standing,&mario1standing,&mario1standing,&mario1standing,
+    &mario1standing,&mario1standing,&mario1standing,&mario1standing,&mario1standing,
+    &mario1standing,&mario1standing,&mario1standing,&mario1standing,&mario1standing,
+    &mario1standing,&mario1standing,&mario1standing,&mario1standing,&mario1standing,
+    &mario1standing,&mario1standing,&mario1standing,&mario1standing,&mario1standing,
+    &mario1standing,&mario1standing,&mario1standing,&mario1standing,&mario1standing,
+    &mario1standing,&mario1standing,&mario1standing,&mario1standing,&mario1standing,
+    &mario1standing,&mario1standing,&mario1standing,&mario1standing,&mario1standing,
+    &mario1standing,&mario1standing,&mario1standing,&mario1standing,&mario1standing,
+    &mario1standing,&mario1standing,&mario1standing,&mario1standing,&mario1standing,
+    &mario2run1,&mario2run2,&mario2run3,
+    &mario2run1,&mario2run2,&mario2run3,
+    &mario2run1,&mario2run2,&mario2run3,
+    &mario2run1,&mario2run2,&mario2run3,
+    &mario2run1,&mario2run2,&mario2run3,
+    &mario3jump1,
+    &mario3jump2,
+    &mario3jump3,
+    &mario3jump4,
+    &mario3jump5,
+    &mario3jump6,
+    &mario3jump7,
+    &mario3jump8,
+    &mario3jump9,
+    &mario3jump10,
+    &mario3jump11,
+    &mario3jump12,
+    &mario3jump13,
 };
 
 void draw_animation(lv_obj_t *canvas) {
@@ -30,8 +56,8 @@ void draw_animation(lv_obj_t *canvas) {
     lv_obj_t *art = lv_animimg_create(canvas);
     lv_obj_center(art);
 
-    lv_animimg_set_src(art, (const void **)anim_imgs, 16);
-    lv_animimg_set_duration(art, CONFIG_NICE_VIEW_GEM_ANIMATION_MS);
+    lv_animimg_set_src(art, (const void **)anim_imgs, 78);
+    lv_animimg_set_duration(art, 7800);
     lv_animimg_set_repeat_count(art, LV_ANIM_REPEAT_INFINITE);
     lv_animimg_start(art);
 #else
