b4852718d093b3fd29513cc806d4b29fae65e1ce
nic <christoph.niederer99@gmail.com>
2026-03-29 11:12:59 +0200

    fix: build

--- boards/shields/nice_view_gem/widgets/animation.h
+++ boards/shields/nice_view_gem/widgets/animation.h
@@ -4,4 +4,10 @@
 #include "util.h"
 #include "screen_peripheral.h"
 
+extern const lv_img_dsc_t bongocatrest0;
+extern const lv_img_dsc_t bongocatcasual1;
+extern const lv_img_dsc_t bongocatcasual2;
+extern const lv_img_dsc_t bongocatfast1;
+extern const lv_img_dsc_t bongocatfast2;
+
 void draw_animation(lv_obj_t *canvas);
