508024f4127b36200c9795e47b5fdffec5a7e991
nic <christoph.niederer99@gmail.com>
2026-03-29 11:16:46 +0200

    fix

--- boards/shields/nice_view_gem/widgets/animation.c
+++ boards/shields/nice_view_gem/widgets/animation.c
@@ -3,11 +3,11 @@
 #include "animation.h"
 
 
-LV_IMG_DECLARE(bongocatrest0);
-LV_IMG_DECLARE(bongocatcasual1);
-LV_IMG_DECLARE(bongocatcasual2);
-LV_IMG_DECLARE(bongocatfast1);
-LV_IMG_DECLARE(bongocatfast2);
+// LV_IMG_DECLARE(bongocatrest0);
+// LV_IMG_DECLARE(bongocatcasual1);
+// LV_IMG_DECLARE(bongocatcasual2);
+// LV_IMG_DECLARE(bongocatfast1);
+// LV_IMG_DECLARE(bongocatfast2);
 LV_IMG_DECLARE(bongo_resting);
 LV_IMG_DECLARE(bongo_casualright);
 LV_IMG_DECLARE(bongo_casualleft);
@@ -17,8 +17,8 @@ LV_IMG_DECLARE(bongo_inhale);
 LV_IMG_DECLARE(bongo_exhale);
 
 const lv_img_dsc_t *anim_imgs[] = {
-    &bongocatrest0, &bongocatcasual1, &bongocatcasual2, &bongocatfast1, &bongocatfast2, &bongo_resting,
-    &bongo_casualright, &bongo_casualleft, &bongo_furiousup, &bongo_furiousdown, &bongo_inhale, &bongo_exhale,
+    // &bongocatrest0, &bongocatcasual1, &bongocatcasual2, &bongocatfast1, &bongocatfast2, 
+    &bongo_resting, &bongo_casualright, &bongo_casualleft, &bongo_furiousup, &bongo_furiousdown, &bongo_inhale, &bongo_exhale,
 };
 
 void draw_animation(lv_obj_t *canvas) {
@@ -26,7 +26,7 @@ void draw_animation(lv_obj_t *canvas) {
     lv_obj_t *art = lv_animimg_create(canvas);
     lv_obj_center(art);
 
-    lv_animimg_set_src(art, (const void **)anim_imgs, 12);
+    lv_animimg_set_src(art, (const void **)anim_imgs, 7);
     lv_animimg_set_duration(art, CONFIG_NICE_VIEW_GEM_ANIMATION_MS);
     lv_animimg_set_repeat_count(art, LV_ANIM_REPEAT_INFINITE);
     lv_animimg_start(art);
